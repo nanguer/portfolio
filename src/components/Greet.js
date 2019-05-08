688829c18bbe4937e19f95e4ef66573086eba9a3
nanguer <nahuelgimenez18@gmail.com>
2019-05-08 23:26:28 +0200

    integrate gsap

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Tween } from 'react-gsap';
 
-const Greet = () => (
-	<Tween from={{ opacity: 0 }} playState="paused">
-		<div>
-			<div className="main-title">
-				<h1>
-					Hi,
-					<br /> I'm <span className="nahuel">Nahuel</span>
-				</h1>
-				<h2>I'm a Web Developer</h2>
-			</div>
-		</div>
-	</Tween>
-);
+class Greet extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	tween;
+	state = {
+		playState: 'pause'
+	};
+	handleClick() {
+		const tween = this.tween.getGSAP();
+		tween.timeScale(0.5);
+		this.setState({ playState: 'play' });
+	}
+
+	render() {
+		return (
+			<Tween to={{ color: 'red' }} playState={this.state.playState} ref={ref => (this.tween = ref)}>
+				<div>
+					<div className="main-title">
+						<h1>
+							Hi,
+							<br /> I'm
+							<span className="nahuel" onClick={this.handleClick}>
+								{' '}
+								Nahuel
+							</span>
+						</h1>
+						<h2>I'am a Web Developer</h2>
+					</div>
+				</div>
+			</Tween>
+		);
+	}
+}
 
 export default Greet;
--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Submenu = () => (
-  <div className="submenu">
-    <NavLink to="/" exact={true}>
-      home
-    </NavLink>
-    <NavLink to="/about" exact={true}>
-      about me
-    </NavLink>
-    <NavLink to="/portfolio" exact={true}>
-      portfolio
-    </NavLink>
-    <NavLink to="/contact" exact={true}>
-      contact me
-    </NavLink>
-  </div>
-);
+const Submenu = () => {
+	return (
+		<div className="submenu">
+			<NavLink to="/" exact={true}>
+				home
+			</NavLink>
+			<NavLink to="/about" exact={true}>
+				about me
+			</NavLink>
+			<NavLink to="/portfolio" exact={true}>
+				portfolio
+			</NavLink>
+			<NavLink to="/contact" exact={true}>
+				contact me
+			</NavLink>
+		</div>
+	);
+};
 
 export default Submenu;
