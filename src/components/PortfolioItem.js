b7ac9814ac7a233ff0cb62f7a7a8c288263debfc
Nahuel <nahuelgimenez18@gmail.com>
2020-01-01 16:07:51 +0100

    works page

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,12 +1,25 @@
 import React from "react";
+import {FaGithub, FaExternalLinkAlt} from "react-icons/fa";
 
-const PortfolioItem = ({ work, ...props }) => {
-  const { title, img, description } = work;
+
+const PortfolioItem = ({ work }) => {
+  const { title, img, description, link, github } = work;
+  const linkIcons = [
+    {Icon: FaExternalLinkAlt, name: 'link', link},
+    {Icon: FaGithub, name: 'github', link: github}
+
+  ]
   return (
-    <div className="portfolio-item d-flex align-items-start flex-column">
-      <h2>{title}</h2>
-      <img src={img} alt="" />
+    <div className="portfolio-item d-flex align-items-center text-center flex-column col-12 col-md-6 col-sm-6" style={{padding:"5vh 1vh"}}>
+      <h2 style={{color:'#fff'}}>{title}</h2>
+      <img src={img} alt="" style={{padding:"4vh 0"}} />
       <p>{description}</p>
+      <div style={{zIndex:"2"}} className="project-links d-flex flex-row justify-content-center">
+        {linkIcons.map(({Icon, name, link})=>(
+          <div key={name} ><a className="icon-link" target="_blank" href={link}><Icon size="1.5em"/></a></div>
+        ))}
+        
+      </div>
     </div>
   );
 };
--- src/components/loaders/ItemLoader.js
+++ src/components/loaders/ItemLoader.js
@@ -3,11 +3,11 @@ import React, { Suspense } from "react";
 const PortfolioItem = React.lazy(() => import("../PortfolioItem"));
 
 const ItemLoader = ({ work }) => (
-  <div>
+  <>
     <Suspense fallback={<div className="loader">Loading...</div>}>
       <PortfolioItem work={work} />
     </Suspense>
-  </div>
+  </>
 );
 
 export default ItemLoader;
