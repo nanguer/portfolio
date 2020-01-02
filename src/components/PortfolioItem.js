c3ad52fca11e3bb2bbd1bfada31e8b18fdda71b2
nanguer <nahuelgimenez18@gmail.com>
2020-01-02 19:59:34 +0100

    continue works styling

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,24 +1,45 @@
 import React from "react";
-import {FaGithub, FaExternalLinkAlt} from "react-icons/fa";
-
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const PortfolioItem = ({ work }) => {
   const { title, img, description, link, github } = work;
   const linkIcons = [
-    {Icon: FaExternalLinkAlt, name: 'link', link},
-    {Icon: FaGithub, name: 'github', link: github}
+    { Icon: FaExternalLinkAlt, name: "link", link, text: "Visit project" },
+    {
+      Icon: FaGithub,
+      name: "github",
+      link: github,
+      text: "View code in Github"
+    }
+  ];
 
-  ]
   return (
-    <div className="portfolio-item d-flex align-items-center text-center flex-column col-12 col-md-6 col-sm-6" style={{padding:"5vh 1vh"}}>
-      <h2 style={{color:'#fff'}}>{title}</h2>
-      <img src={img} alt="" style={{padding:"4vh 0"}} />
+    <div
+      className="portfolio-item d-flex align-items-center text-center flex-column col-12 col-md-6 col-sm-6"
+      style={{ padding: "5vh 1vh" }}
+    >
+      <h2 style={{ color: "#fff" }}>{title}</h2>
+      <img src={img} alt="" style={{ padding: "4vh 0" }} />
       <p>{description}</p>
-      <div style={{zIndex:"2"}} className="project-links d-flex flex-row justify-content-center">
-        {linkIcons.map(({Icon, name, link})=>(
-          <div key={name} ><a className="icon-link" target="_blank" href={link}><Icon size="1.5em"/></a></div>
+      <div
+        style={{ zIndex: "2" }}
+        className="project-links d-flex flex-row justify-content-center"
+      >
+        {linkIcons.map(({ Icon, name, link, text }) => (
+          <div key={name}>
+            <div className={`project-links__icon-div-${name}`}>
+              <a
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={link}
+              >
+                <Icon size="1.5em" />
+              </a>
+            </div>
+            <div className={`tooltip-works-${name}`}>{text}</div>
+          </div>
         ))}
-        
       </div>
     </div>
   );
