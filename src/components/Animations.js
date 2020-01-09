28b9f1b3aff65ba22282da5976be47791c5edae1
Nahuel Nicolas Gimenez (WT65-IT ServicesPolandsp.z o.o) <NA20050369@wipro.com>
2020-01-09 18:54:22 +0100

    work on navbar animation

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -1,171 +1,226 @@
 import gsap from "gsap";
 
 const tlStripes = gsap.timeline();
-const tlNavbar = gsap.timeline();
 
-export const animateMenu = (target, direction) => {
-  if (!direction) {
-    return tlNavbar.to(target, {
-      x: 0,
-      duration: 0.5,
-      opacity: 1,
-      ease: "power3.out",
-      stagger: {
-        ease: "power3.out",
-        amount: 0.7
-      }
-    });
-  } else {
-    return tlNavbar.to(target, {
-      x: 320,
-      duration: 0.5,
-      opacity: 0,
-      ease: "power3.out",
-      stagger: {
-        ease: "power3.out",
-        amount: 0.4
-      }
-    });
-  }
+export const constructAnimation = (menuItems, hamburguer) => {
+	const tlNavbar = gsap.timeline({ paused: true });
+	tlNavbar.to(menuItems, {
+		x: 0,
+		duration: 0.5,
+		opacity: 1,
+		stagger: {
+			ease: "power3.out",
+			amount: 0.7
+		}
+	});
+	tlNavbar.to(
+		hamburguer[0],
+		0.2,
+		{
+			x: 1,
+			y: 6,
+			rotation: "45deg"
+		},
+		0.9
+	);
+	tlNavbar.to(
+		hamburguer[1],
+		0.2,
+		{
+			opacity: 0,
+			scale: (0.2, 0.2)
+		},
+		0.9
+	);
+	tlNavbar.to(
+		hamburguer[2],
+		0.2,
+		{
+			x: -1,
+			y: 2,
+			rotation: "-45deg"
+		},
+		0.9
+	);
+
+	return tlNavbar;
 };
 
-export const animateHamb = (target, direction) => {
-  //console.log(target[0]);
-  if (!direction) {
-    gsap.to(target[0], {
-      x: 1,
-      y: 6,
-      rotation: "45deg"
-    });
-  }
+export const animateMenu = (timeline, direction) => {
+	if (!direction) {
+		timeline.current.play();
+	} else {
+		timeline.current.reverse();
+	}
 };
+
+// export const animateHamb = (target, direction) => {
+// 	console.log("animateHamb");
+// 	if (!direction) {
+// 		gsap.to(target[0], 0.2, {
+// 			x: -2,
+// 			y: 4,
+// 			rotation: "45deg",
+// 			ease: "power3.In"
+// 		});
+// 		gsap.to(target[1], 0.2, {
+// 			opacity: 0,
+// 			scale: (0.2, 0.2),
+// 			ease: "power3.In"
+// 		});
+// 		gsap.to(target[2], 0.2, {
+// 			x: -7,
+// 			y: 0,
+// 			rotation: "-45deg",
+// 			ease: "power3.In"
+// 		});
+// 	} else {
+// 		gsap.to(target[0], 0.2, {
+// 			x: 0,
+// 			y: 0,
+// 			rotation: "0deg",
+// 			ease: "power3.In"
+// 		});
+// 		gsap.to(target[1], 0.2, {
+// 			opacity: 1,
+// 			scale: (1, 1),
+// 			ease: "power3.In"
+// 		});
+// 		gsap.to(target[2], 0.2, {
+// 			x: 0,
+// 			y: 0,
+// 			rotation: "0deg",
+// 			ease: "power3.In"
+// 		});
+// 	}
+// };
 
 //Stripes background animation timeline
 export const setTimeLine = (option, target) => {
-  if (option === "About") {
-    return tlStripes.to(target, {
-      x: 300,
-      duration: 1,
-      delay: 1,
-      ease: "power3.out"
-    });
-  } else if (option === "Landing") {
-    return tlStripes.to(target, {
-      x: 0,
-      duration: 1,
-      delay: 1,
-      ease: "power3.out"
-    });
-  } else if (option === "Portfolio") {
-    return tlStripes.to(target, {
-      x: 600,
-      duration: 1,
-      delay: 1,
-      ease: "power3.out"
-    });
-  } else if (option === "Contact") {
-    return tlStripes.to(target, {
-      x: 900,
-      duration: 1,
-      delay: 1,
-      ease: "power3.out"
-    });
-  }
-  return;
+	if (option === "About") {
+		return tlStripes.to(target, {
+			x: 300,
+			duration: 1,
+			delay: 1,
+			ease: "power3.out"
+		});
+	} else if (option === "Landing") {
+		return tlStripes.to(target, {
+			x: 0,
+			duration: 1,
+			delay: 1,
+			ease: "power3.out"
+		});
+	} else if (option === "Portfolio") {
+		return tlStripes.to(target, {
+			x: 600,
+			duration: 1,
+			delay: 1,
+			ease: "power3.out"
+		});
+	} else if (option === "Contact") {
+		return tlStripes.to(target, {
+			x: 900,
+			duration: 1,
+			delay: 1,
+			ease: "power3.out"
+		});
+	}
+	return;
 };
 
 //Landing component Animations
 
 export const setOpacity = target => {
-  gsap.to(target, 0.2, {
-    opacity: 1
-  });
+	gsap.to(target, 0.2, {
+		opacity: 1
+	});
 };
 export const handleEnterTitle = target => {
-  gsap.from(target, 0.2, {
-    opacity: 0,
-    delay: 1.8,
-    ease: "power3.in",
-    x: 25,
-    stagger: {
-      ease: "power3.in",
-      amount: 0.4
-    }
-  });
+	gsap.from(target, 0.2, {
+		opacity: 0,
+		delay: 1.8,
+		ease: "power3.in",
+		x: 25,
+		stagger: {
+			ease: "power3.in",
+			amount: 0.4
+		}
+	});
 };
 
 export const handleExitTitle = target => {
-  gsap.to(target, 0.2, {
-    x: 255,
-    delay: 0.2,
-    opacity: 0,
-    ease: "power3.in",
-    stagger: {
-      ease: "power3.in",
-      amount: 0.5
-    }
-  });
+	gsap.to(target, 0.2, {
+		x: 255,
+		delay: 0.2,
+		opacity: 0,
+		ease: "power3.in",
+		stagger: {
+			ease: "power3.in",
+			amount: 0.5
+		}
+	});
 };
 
 export const enterPicture = picture => {
-  gsap.from(picture, {
-    opacity: 0,
-    delay: 2,
-    ease: "power3.in"
-  });
+	gsap.from(picture, {
+		opacity: 0,
+		delay: 2,
+		ease: "power3.in"
+	});
 };
 
 export const exitPicture = picture => {
-  gsap.to(picture, {
-    x: 35,
-    opacity: 0,
-    delay: 0.3,
-    ease: "power3.out"
-  });
+	gsap.to(picture, {
+		x: 35,
+		opacity: 0,
+		delay: 0.3,
+		ease: "power3.out"
+	});
 };
 
 //Footer Component animations
 export const handleFooterIn = target => {
-  gsap.from(target, 0.2, {
-    opacity: 0,
-    delay: 1.2,
-    x: 55,
-    ease: "power3.in",
-    stagger: {
-      ease: "power3.in",
-      amount: 0.3
-    }
-  });
+	gsap.from(target, 0.2, {
+		opacity: 0,
+		delay: 1.2,
+		x: 55,
+		ease: "power3.in",
+		stagger: {
+			ease: "power3.in",
+			amount: 0.3
+		}
+	});
 };
 
 export const handleFooterOut = target => {
-  gsap.to(target, 0.2, {
-    opacity: 0,
-    x: 55,
-    ease: "power3.in",
-    stagger: {
-      ease: "power3.in",
-      amount: 0.3
-    }
-  });
+	gsap.to(target, 0.2, {
+		opacity: 0,
+		x: 55,
+		ease: "power3.in",
+		stagger: {
+			ease: "power3.in",
+			amount: 0.3
+		}
+	});
 };
 
 //Contact Page Animations
 
 export const animateContactScroll = anchor => {
-  gsap.to(window, 1.3, {
-    duration: 1,
-    ease: "power3.inOut",
-    scrollTo: { y: anchor }
-  });
+	gsap.to(window, 1.3, {
+		duration: 1,
+		ease: "power3.inOut",
+		scrollTo: { y: anchor }
+	});
 };
 
 //spinner
 
 export const spinnerLoad = target => {
-  gsap.from(target, 1, {
-    opacity: 0,
-    scale: -10,
-    ease: "power3.easeIn"
-  });
+	gsap.from(target, 1, {
+		opacity: 0,
+		scale: -10,
+		ease: "power3.easeIn"
+	});
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,98 +1,108 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import { animateMenu, animateHamb } from "./Animations";
+import { animateMenu, constructAnimation } from "./Animations";
 
 export default ({ navstate, handleSetNav }) => {
-  const [isOpen, setOpen] = useState(false);
-  const [disableClick, setDisabled] = useState(false);
-  let menu1 = useRef(null);
-  let menu2 = useRef(null);
-  let menu3 = useRef(null);
-  let menu4 = useRef(null);
-  let span1 = useRef(null);
-  let span2 = useRef(null);
-  let span3 = useRef(null);
+	const [isOpen, setOpen] = useState(false);
+	const [disableClick, setDisabled] = useState(false);
+	let menu1 = useRef(null);
+	let menu2 = useRef(null);
+	let menu3 = useRef(null);
+	let menu4 = useRef(null);
+	let span1 = useRef(null);
+	let span2 = useRef(null);
+	let span3 = useRef(null);
 
-  const toggle = booleanValue => {
-    animateHamb([span1, span2, span3], booleanValue);
-    animateMenu([menu1, menu2, menu3, menu4], booleanValue);
-    setDisabled(true);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 1000);
-    setOpen(!isOpen);
-  };
+	let timeline = useRef(null);
 
-  const handleNavClick = (option, booleanValue) => {
-    handleSetNav(option);
-    toggle(booleanValue);
-  };
+	useEffect(() => {
+		timeline.current = constructAnimation(
+			[menu1, menu2, menu3, menu4],
+			[span1, span2, span3]
+		);
+	}, []);
 
- 
-  return (
-    <nav role="navigation">
-      <div className="container-fluid menu d-flex flex-row position-absolute justify-content-end pt-2">
-        <div id="menuToggle">
-          <div
-            className="checkbox-container d-flex justify-content-end flex-row"
-            style={{ height: "2rem" }}
-          >
-            <input
-              disabled={disableClick}
-              type="checkbox"
-              onClick={() => toggle(isOpen)}
-            />
-            <span ref={el => span3 = el}></span>
-            <span ref={el => span2 = el}></span>
-            <span ref={el => span1 = el}></span>
-          </div>
-          <ul
-            id="menuHamb"
-            className="d-flex flex-row position-absolute"
-            style={{ right: "34px", top: "-10px" }}
-          >
-            <NavLink
-              to="/"
-              exact={true}
-              activeClassName="active"
-              onClick={() => handleNavClick("Landing", isOpen)}
-            >
-              <li ref={el => (menu1 = el)} className="hambHome">
-                Home
-              </li>
-            </NavLink>
-            <NavLink
-              to="/about"
-              onClick={() => handleNavClick("About", isOpen)}
-              exact={true}
-            >
-              <li ref={el => (menu2 = el)} className="hambAbout">
-                {" "}
-                About
-              </li>
-            </NavLink>
-            <NavLink
-              to="/portfolio"
-              exact={true}
-              onClick={() => handleNavClick("Portfolio", isOpen)}
-            >
-              <li ref={el => (menu3 = el)} className="hambWorks">
-                Works
-              </li>
-            </NavLink>
-            <NavLink
-              to="/contact"
-              onClick={() => handleNavClick("Contact", isOpen)}
-              exact={true}
-            >
-              <li ref={el => (menu4 = el)} className="hambContact">
-                {" "}
-                Contact
-              </li>
-            </NavLink>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	const toggle = booleanValue => {
+		animateMenu(timeline, booleanValue);
+		setDisabled(true);
+		setTimeout(() => {
+			setDisabled(false);
+		}, 1000);
+		setOpen(!isOpen);
+	};
+
+	const handleNavClick = (option, booleanValue) => {
+		handleSetNav(option);
+		toggle(booleanValue);
+	};
+
+	return (
+		<nav role="navigation">
+			<div className="container-fluid menu d-flex flex-row position-absolute justify-content-end pt-2">
+				<div id="menuToggle">
+					<div
+						className="checkbox-container d-flex justify-content-end flex-row"
+						style={{ height: "2rem" }}
+					>
+						<input
+							disabled={disableClick}
+							type="checkbox"
+							onClick={() => toggle(isOpen)}
+						/>
+						<span ref={el => (span3 = el)}></span>
+						<span ref={el => (span2 = el)}></span>
+						<span ref={el => (span1 = el)}></span>
+					</div>
+					<ul
+						id="menuHamb"
+						className="d-flex flex-row position-absolute"
+						style={{ right: "34px", top: "-10px" }}
+					>
+						<NavLink
+							to="/"
+							exact={true}
+							activeClassName="active"
+							onClick={() => handleNavClick("Landing", isOpen)}
+						>
+							<li ref={el => (menu1 = el)} className="hambHome">
+								Home
+							</li>
+						</NavLink>
+						<NavLink
+							to="/about"
+							onClick={() => handleNavClick("About", isOpen)}
+							exact={true}
+						>
+							<li ref={el => (menu2 = el)} className="hambAbout">
+								{" "}
+								About
+							</li>
+						</NavLink>
+						<NavLink
+							to="/portfolio"
+							exact={true}
+							onClick={() => handleNavClick("Portfolio", isOpen)}
+						>
+							<li ref={el => (menu3 = el)} className="hambWorks">
+								Works
+							</li>
+						</NavLink>
+						<NavLink
+							to="/contact"
+							onClick={() => handleNavClick("Contact", isOpen)}
+							exact={true}
+						>
+							<li
+								ref={el => (menu4 = el)}
+								className="hambContact"
+							>
+								{" "}
+								Contact
+							</li>
+						</NavLink>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
