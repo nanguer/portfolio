24f7e6c131f859bc564e430caaba6f29c2b1af40
nanguer <nngimenez1985@gmail.com>
2020-07-29 07:57:05 +0200

    add meta tag component

---
+++ src/Meta.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MetaTags from 'react-meta-tags';
+
+const Meta = () => {
+    return (
+       <MetaTags>
+       <meta property='og:title' content='Nahuel Gimenez | Web Developer'/>
+    <meta property='og:image' content='../assets/me-bg.jpg'/>
+    <meta property='og:description' content='Personal Website and Portfolio'/>
+    <meta property='og:url' content='//nahueldev.me'/>
+       </MetaTags>
+    )
+}
+
+export default Meta;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import AppRouter from './routers/AppRouter';
 import { AppContextProvider } from '../src/context/AppContext';
 import './styles/styles.scss';
 import * as serviceWorker from './serviceWorker';
+import Meta from './Meta';
 
 ReactDOM.render(
   <AppContextProvider>
+    <Meta />
     <AppRouter />
   </AppContextProvider>,
   document.getElementById('root')
